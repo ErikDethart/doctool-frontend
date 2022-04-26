04e7108773c862204e86264af64243fa5ce40079
Erik Dethart <mail@erikdethart.com>
2022-04-25 21:29:39 -0500

    Setup navigation, begin implement REST api client.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,25 +5,38 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { RouterModule } from '@angular/router';
+
+import { HttpClientModule } from '@angular/common/http';
+
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatListModule, MatNavList} from '@angular/material/list';
+import { TeamviewerListComponent } from './teamviewer-list/teamviewer-list.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TeamviewerListComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot([
+      {path: 'teamviewer', component: TeamviewerListComponent},
+      {path: '', component: HomepageComponent}
+    ]),
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     MatSidenavModule,
     MatIconModule,
     MatButtonModule,
-    MatListModule
+    MatListModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/teamviewer-list/teamviewer-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-teamviewer-list',
+  templateUrl: './teamviewer-list.component.html',
+  styleUrls: ['./teamviewer-list.component.sass']
+})
+export class TeamviewerListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
